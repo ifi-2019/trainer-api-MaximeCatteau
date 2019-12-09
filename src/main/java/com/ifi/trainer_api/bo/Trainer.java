b6c25e0f2d6a1845d6583b045254eabde47a4961
Catteau Maxime <maxime.catteau.etu@univ-lille.fr>
2019-12-09 10:22:07 +0100

    Password encryption

--- src/main/java/com/ifi/trainer_api/bo/Trainer.java
+++ src/main/java/com/ifi/trainer_api/bo/Trainer.java
@@ -1,5 +1,6 @@
 package com.ifi.trainer_api.bo;
 
+import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -10,6 +11,9 @@ public class Trainer {
 
     @Id
     private String name;
+
+    @Column
+    private String password;
 
     @ElementCollection
     private List<Pokemon> team;
@@ -36,5 +40,13 @@ public class Trainer {
 
     public void setTeam(List<Pokemon> team) {
         this.team = team;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
